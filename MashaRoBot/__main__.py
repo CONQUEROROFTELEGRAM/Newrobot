a22f642cdf613958d47867658b178937e2cdeff5
♣️♣️٠•●♥♗ⓐⓃⓔⒺⓢⒽⓟⒶⓝⒶⓚⓀⓐⓁ♗♥●•٠♣️♣️ <79249487+CONQUEROROFTELEGRAM@users.noreply.github.com>
2021-06-29 23:03:41 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -97,7 +97,7 @@ buttons = [
 HELP_STRINGS = """
 Welcome to the help menu!
 """
-MASHA_IMG = "https://i.imgur.com/0aeced684a2ae051d7b58.jpg"
+MASHAROBOT_IMG = "https://i.imgur.com/0aeced684a2ae051d7b58.jpg"
 
 DONATE_STRING = """No need.. I'm rich"""
 

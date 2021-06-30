98ea2eaa82cc667bff323a2b25d0cbe084ec0b4c
♣️♣️٠•●♥♗ⓐⓃⓔⒺⓢⒽⓟⒶⓝⒶⓚⓀⓐⓁ♗♥●•٠♣️♣️ <79249487+CONQUEROROFTELEGRAM@users.noreply.github.com>
2021-06-30 07:19:56 +0000

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -289,7 +289,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "「 *Help for* *{}* 」\n".format(
+                " *Help for* *{}* \n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
